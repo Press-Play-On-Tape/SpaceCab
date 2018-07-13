5247a54e2e79fcbc3bd37af9bccf56b16c040dab
Stephane C <30958385+vampirics@users.noreply.github.com>
2018-07-13 09:00:07 -0400

    All Player starting position fixed!

--- SpaceCab/src/Entities/Levels.h
+++ SpaceCab/src/Entities/Levels.h
@@ -13,15 +13,17 @@
 #define LEVEL_0_HEIGHT 8
 
 const uint8_t PROGMEM level0Map[] = {
-0x5F, 0x51, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x06, 0xA2, 0x41, 0xAA, 0x41, 0xA2, 0x00
+0x66, 0x54, 0x66, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 
+0x08, 0xA8, 0x41, 0xA1, 0x03, 0x33, 0x8A, 0x33, 
+0x00
 };
 
 const uint8_t PROGMEM level0StartingPositions[4] = {
-4, 7, 11, 7, 
+4, 6, 12, 6, 
 };
 
 const uint8_t PROGMEM level0EndingPositions[4] = {
-2, 7, 13, 7, 
+2, 6, 11, 6, 
 };
 
 
@@ -269,16 +271,16 @@ const int8_t levelInit[] = {
 
 // Level width   Level Height,   Level     Level     Player  Player   Fares      Name     Number Of
 //                               X Offset  Y Offset  X Pos   Y Pos   Required   Offset    Positions
-  LEVEL_0_WIDTH, LEVEL_0_HEIGHT,       0,        0,     18,     53,     0,         0,         2,
-  LEVEL_1_WIDTH, LEVEL_1_HEIGHT,     -54,      -56,     56,     45,     1,        16,         6,
-  LEVEL_2_WIDTH, LEVEL_2_HEIGHT,     -54,      -56,     56,     45,     0,        12,         4,
-  LEVEL_3_WIDTH, LEVEL_3_HEIGHT,     127,      -56,     56,     29,     0,        10,         4,
-  LEVEL_4_WIDTH, LEVEL_4_HEIGHT,     -32,      -56,     56,     13,     0,        12,         4,
-  LEVEL_5_WIDTH, LEVEL_5_HEIGHT,     -54,      -56,     56,     45,     0,        16,         1,
-  LEVEL_6_WIDTH, LEVEL_6_HEIGHT,     -54,      -56,     56,     45,     0,        10,         4,
-  LEVEL_7_WIDTH, LEVEL_7_HEIGHT,     -54,      -56,     56,     45,     0,        16,         4,
-  LEVEL_8_WIDTH, LEVEL_8_HEIGHT,     -54,      -56,     56,     45,     0,        10,         4,
-  LEVEL_9_WIDTH, LEVEL_9_HEIGHT,     -54,      -56,     56,     45,     0,        16,         4,
+  LEVEL_0_WIDTH, LEVEL_0_HEIGHT,       0,        0,     28,     45,     0,         0,         2,
+  LEVEL_1_WIDTH, LEVEL_1_HEIGHT,     -107,      0,     56,     21,     0,        16,         6,
+  LEVEL_2_WIDTH, LEVEL_2_HEIGHT,     -44,      -56,     65,     45,     0,        12,         4,
+  LEVEL_3_WIDTH, LEVEL_3_HEIGHT,     -107,     -56,     56,     29,     0,        10,         4,
+  LEVEL_4_WIDTH, LEVEL_4_HEIGHT,     -36,      -41,     56,     28,     0,        12,         4,
+  LEVEL_5_WIDTH, LEVEL_5_HEIGHT,     0,      0,     29,     21,     0,        16,         2,
+  LEVEL_6_WIDTH, LEVEL_6_HEIGHT,     -39,      -33,     56,     28,     0,          9,         4,
+  LEVEL_7_WIDTH, LEVEL_7_HEIGHT,     -144,      -56,     59,     45,     0,        16,         4,
+  LEVEL_8_WIDTH, LEVEL_8_HEIGHT,     -58,      -1,     56,     28,     0,        10,         4,
+  LEVEL_9_WIDTH, LEVEL_9_HEIGHT,     -140,      -25,     56,     28,     0,        16,         4,
 };
 
 char const * const levelNames[] PROGMEM = {
